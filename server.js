873e45c011386ab93c908cef23e8491e5f99342e
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2023-10-31 12:11:19 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8081 });
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+
+                client.send(message.toString());
+            }
+        });
+    });
+
+    ws.on('close', () => {
+    });
+});
