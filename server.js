bed6c2cd4af0f5dae720a58353e9c82ef3807b2d
Md Shahriar Kabir <68655623+shahriarKabir44@users.noreply.github.com>
2023-11-25 01:55:26 +0600

    added express

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
+
+const express = require('express');
+const http = require('http');
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8081 });
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello, this is a WebSocket server!');
+});
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
-    ws.on('message', (message) => {
-        wss.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
 
-                client.send(message.toString());
-            }
-        });
-    });
+	ws.on('message', (message) => {
+		wss.clients.forEach((client) => {
+			if (client !== ws && client.readyState === WebSocket.OPEN) {
+				client.send(message);
+			}
+		});
+	});
 
-    ws.on('close', () => {
-    });
+	ws.on('close', () => {
+	});
 });
+
+server.listen(process.env.PORT);
